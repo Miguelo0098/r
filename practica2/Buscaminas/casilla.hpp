aa2bc33fa23408efbb85e1dd2aace8871cf356a7
Miguelo0098 <i62rarum@uco.es>
2018-10-08 15:10:47 +0200

    Codificada clase Casilla

---
+++ practica2/Buscaminas/casilla.hpp
@@ -0,0 +1,47 @@
+#ifndef CASILLA_HPP
+#define CASILLA_HPP
+#endif
+
+class Casilla {
+private:
+  int n_bombs;
+  bool bomb;
+  bool mark;
+
+public:
+  /* Constructor */
+  inline Casilla (int nBombas = 0, bool bomba = false, bool marca = false){
+    setNBombs(nBombas);
+    setBomb(bomba);
+    setMark(marca);
+  }
+
+  /* Observadores */
+  inline int getNBombs(){
+    return n_bombs;
+  }
+
+  inline bool hasBomb(){
+    return bomb;
+  }
+
+  inline bool hasMark(){
+    return mark;
+  }
+
+  /* Modificadores */
+
+  inline void setNBombs(int nBombas){
+    n_bombs = nBombas;
+  }
+
+  inline void setBomb(bool bomba){
+    bomb = bomba;
+  }
+
+  inline void setMark(bool marca){
+    mark = marca;
+  }
+
+
+};
