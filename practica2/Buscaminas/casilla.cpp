ffafb4bcc3b1380ee2c470822688cbbe6268862f
V1ckyxd <36993779+V1ckyxd@users.noreply.github.com>
2018-10-08 16:59:23 +0200

    printCasilla hecho

--- practica2/Buscaminas/casilla.cpp
+++ practica2/Buscaminas/casilla.cpp
@@ -1,1 +1,25 @@
 #include "casilla.hpp"
+#include <iostream>
+
+void Casilla::printCasilla()const{
+  if(isCovered()){ //Esta cubierta
+    if(!getMark()) //No tiene ninguna bandera
+      printf("-");
+    else{
+      if(getMark() == 1){ //Bandera de jugador A
+        printf("A");
+      }else{
+        if(getMark() == 2){ //Bandera de jugador B
+          printf("B");
+        }else //Bandera de ambos
+          printf("A|B");
+      }
+    }
+  }else{ // Esta descubierta
+    if(hasBomb()) // Tiene bomba
+      printf("*");
+    else{ //No tiene bomba
+      printf(getNBombs());
+    }
+  }
+}
