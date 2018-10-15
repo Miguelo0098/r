e8fee76d1e0323dcab2b1a779f67750701be6a3a
Miguelo0098 <i62rarum@uco.es>
2018-10-15 17:05:56 +0200

    función reset terminada

--- practica2/Buscaminas/tablero.cpp
+++ practica2/Buscaminas/tablero.cpp
@@ -58,14 +58,84 @@ void Tablero::resetTablero(){
 
   /* Contamos el número de bombas alrededor de cada casilla */
   int n_bombs = 0;
+
+  /* Para las 64 bombas del centro */
   for (int i = 1; i < 9; i++) {
     for (int j = 1; j < 9; j++) {
       n_bombs = 0;
       for (int ip = -1; ip < 2; ip++) {
         for (int jp = -1; jp < 2; jp++) {
-          /* code */
+          if (getCasilla(i+ip, j+jp).hasBomb() == true) {
+            n_bombs++;
+          }
+        }
+      }
+      getCasilla(i, j).setNBombs(n_bombs);
+    }
+  }
+
+  /* Para los bordes de arriba e izquierda */
+  int n_bombs2 = 0
+  for (int i = 1; i < 10; i++) {
+    n_bombs = 0;
+    n_bombs2 = 0;
+    for (int ip = -1; ip < 2; ip++) {
+      for (int jp = 0; jp < 2; jp++) {
+        if (getCasilla(i+ip, jp).hasBomb() == true) {
+          n_bombs++;
+        }
+        if (getCasilla(jp, i+ip).hasBomb() == true) {
+          n_bombs2++;
         }
       }
     }
+    getCasilla(i, 0).setNBombs(n_bombs);
+    getCasilla(0, i).setNBombs(n_bombs2);
   }
+
+  /* Para los bordes de abajo y derecha */
+  for (int i = 1; i < 10; i++) {
+    n_bombs = 0;
+    n_bombs2 = 0;
+    for (int ip = -1; ip < 2; ip++) {
+      for (int jp = 9; jp < 11; jp++) {
+        if (getCasilla(i+ip, jp).hasBomb() == true) {
+          n_bombs++;
+        }
+        if (getCasilla(jp, i+ip).hasBomb() == true) {
+          n_bombs2++;
+        }
+      }
+    }
+    getCasilla(i, 10).setNBombs(n_bombs);
+    getCasilla(10, i).setNBombs(n_bombs2);
+  }
+
+  /* Rellenamos las esquinas */
+  n_bombs = 0;
+  n_bombs2 = 0;
+  int n_bombs3 = 0;
+  int n_bombs4 = 0;
+
+  for (i = 0; i < 2; i++) {
+    for (j = 0; j < 2; j++) {
+      if (getCasilla(i, j).hasBomb() == true) {
+        n_bombs++;
+      }
+      if (getCasilla(10-i, j).hasBomb() == true) {
+        n_bombs2++;
+      }
+      if (getCasilla(i, 10-j).hasBomb() == true) {
+        n_bombs3++;
+      }
+      if (getCasilla(10-i, 10-j).hasBomb() == true) {
+        n_bombs4++;
+      }
+    }
+  }
+  getCasilla(0, 0).setNBombs(n_bombs);
+  getCasilla(10, 0).setNBombs(n_bombs2);
+  getCasilla(0, 10).setNBombs(n_bombs3);
+  getCasilla(10, 10).setNBombs(n_bombs4);
+
 }
