14a1c4b537f44de45ac6aa8d0deec11cef271e04
Miguelo0098 <i62rarum@uco.es>
2018-10-22 10:09:20 +0200

    Modificadas librerias

--- practica2/Buscaminas/cliente.cpp
+++ practica2/Buscaminas/cliente.cpp
@@ -1,13 +1,13 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <time.h>
-#include <arpa/inet.h>
+#include <cstdio>
+#include <csys/types>
+#include <csys/socket>
+#include <cnetinet/in>
+#include <cnetdb>
+#include <cstdlib>
+#include <cstring>
+#include <cunistd>
+#include <ctime>
+#include <carpa/inet>
 #include "tablero.hpp"
 
 #define SERVER_HOST "localhost"
