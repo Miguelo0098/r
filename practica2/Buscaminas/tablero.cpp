08085c35600866a886eb51a70d403430d628dab4
Miguelo0098 <i62rarum@uco.es>
2018-10-28 17:45:15 +0100

    Terminada funciones win a y b

--- practica2/Buscaminas/tablero.cpp
+++ practica2/Buscaminas/tablero.cpp
@@ -42,11 +42,37 @@ int Tablero::getMarksTabB(){
 }
 
 bool Tablero::getWinA(){
-
+    int count;
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        if (getCasilla(i, j).getMark() == 1) {
+          if (getCasilla(i,j).hasBomb() == true) {
+              count++;
+          }
+        }
+      }
+    }
+    if (count == 10) {
+        return true;
+    }
+    return false;
 }
 
 bool Tablero::getWinB(){
-    
+    int count;
+    for (int i = 0; i < 10; i++) {
+      for (int j = 0; j < 10; j++) {
+        if (getCasilla(i, j).getMark() == 2) {
+          if (getCasilla(i,j).hasBomb() == true) {
+              count++;
+          }
+        }
+      }
+    }
+    if (count == 10) {
+        return true;
+    }
+    return false;
 }
 
 void Tablero::resetTablero(){
